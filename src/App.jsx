1b914421990474ae336f6415e49322d0a5e1cd5d
ChengAaron <cheng.aaron@outlook.com>
2025-01-04 12:31:48 -0500

    added stuff

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,43 @@ const text = [
   {desc: "also known as chengs on discord"}
 ]
 
-//const links = []
+const importantLinks = [
+  {title: "GitHub", link: "https://github.com/ChengAaron"},
+  {title: "LinkedIn", link: "https://www.linkedin.com/in/aaron-cheng-70a091195/"},
+  {title: "Email", link: "mailto:cheng.aaron@proton.me"},
+  {title: "Devpost", link: "https://devpost.com/Chengs"},
+  {title: "TCG Twitter", link: "https://x.com/imchengs"},
+]
+
+const importantLinksList = importantLinks.map(
+  (link) => <li className="text-secondary-content text-xl pl-2 pr-2"><a href={link.link} target="_blank" className="link link-hover">{link.title}</a></li>
+);
+
+function LinksList () {
+  return (
+    <div className="justify-center items-center col-span-2">
+      <div className="justify-self-auto m-8 -mb-8 -mt-4">
+        <p className="text-accent text-3xl mt-1">External Pages</p>
+        <p className="text-secondary-content text-xl">Other places to find me</p>
+        <div className="divider divider-neutral m-0"></div>
+        <ul className="list-disc list-inside columns-3">
+          {importantLinksList}
+        </ul>
+      </div>
+    </div>
+  );
+}
 
 function Title({ firstName, lastName, desc }) {
   return (
     <div className="justify-center">
-    <h1 className="text-7xl">
-      {firstName}<br />{lastName}
-    </h1>
-    <p className="text-l text-secondary mt-0.5">
-      {desc}
-    </p>
+      <h1 className="text-7xl text-primary-content">
+        {firstName}<br />{lastName}
+      </h1>
+      <p className="text-l text-primary-content mt-0.5">
+        {desc}
+      </p>
+      <ColorChange />
     </div>
   );
 }
@@ -23,33 +49,93 @@ function Title({ firstName, lastName, desc }) {
 function Bio({ bio }) {
   return (
     <div className="col-span-2">
-      <p className="text-xl text-center text-primary">
+      <p className="text-xl text-center text-secondary-content">
       {bio}
       </p>
-      <div className="divider divider-primary m-0.5"></div>
+      <div className="divider divider-neutral m-0"></div>
     </div>
   );
 }
 
 function ColorChange() {
-
-}
-
-/*
-function links() {
   return (
-    <></>
+    <div className="dropdown mb-72">
+      <div tabIndex={0} role="button" className="btn text-primary-content bg-primary">
+        Keycaps
+        <svg
+          width="12px"
+          height="12px"
+          className="inline-block h-2 w-2 fill-current opacity-60"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 2048 2048">
+          <path d="M1799 349l242 241-1017 1017L7 590l242-241 775 775 775-775z"></path>
+        </svg>
+      </div>
+      <ul tabIndex={0} className="dropdown-content bg-primary rounded-box z-[1] w-52 p-2 shadow-2xl overflow-y-scroll">
+        <li>
+          <input
+            type="radio"
+            name="theme-dropdown"
+            className="theme-controller btn btn-sm btn-block btn-ghost justify-start text-primary-content"
+            aria-label="Oblivion"
+            value="oblivion" />
+        </li>
+        <li>
+          <input
+            type="radio"
+            name="theme-dropdown"
+            className="theme-controller btn btn-sm btn-block btn-ghost justify-start text-primary-content"
+            aria-label="Olivia"
+            value="olivia" />
+        </li>
+        <li>
+          <input
+            type="radio"
+            name="theme-dropdown"
+            className="theme-controller btn btn-sm btn-block btn-ghost justify-start text-primary-content"
+            aria-label="Botanical"
+            value="botanical" />
+        </li>
+        <li>
+          <input
+            type="radio"
+            name="theme-dropdown"
+            className="theme-controller btn btn-sm btn-block btn-ghost justify-start text-primary-content"
+            aria-label="Nimbus"
+            value="nimbus" />
+        </li>
+        <li>
+          <input
+            type="radio"
+            name="theme-dropdown"
+            className="theme-controller btn btn-sm btn-block btn-ghost justify-start text-primary-content"
+            aria-label="Delta"
+            value="delta" />
+        </li>
+        <li>
+          <input
+            type="radio"
+            name="theme-dropdown"
+            className="theme-controller btn btn-sm btn-block btn-ghost justify-start text-primary-content"
+            aria-label="Mizu"
+            value="mizu" />
+        </li>
+      </ul>
+    </div>
   );
 }
-*/
+
 function GridLayout() {
   return (
     <div className="grid grid-cols-3 min-h-screen bg-secondary">
-        <div className="grid grid-cols-subgrid text-secondary bg-primary place-items-center">
+        <div className="grid grid-cols-subgrid bg-primary place-items-center">
           <Title firstName={text[0].firstName} lastName={text[1].lastName} desc={text[3].desc}/>
         </div>
         <div className="grid grid-cols-subgrid col-span-2 m-4">
           <Bio bio={text[2].bio} />
+          <LinksList />
+          <LinksList />
+          <LinksList />
         </div>
     </div>
   );
