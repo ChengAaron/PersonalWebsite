c50671c678bcfe3763dc96d7ca7c3bc562ce9f2e
ChengAaron <cheng.aaron@outlook.com>
2025-01-19 05:44:50 -0500

    analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import { Analytics } from "@vercel/analytics/react"
 import { BrowserRouter as Router, Routes, Route, BrowserRouter } from "react-router";
 import HomePage from "./pages/home"
 
@@ -6,6 +7,7 @@ export default function App() {
   document.documentElement.setAttribute("data-theme", localStorage.getItem("current"))
   return (
     <BrowserRouter>
+      <Analytics />
       <Routes>
         <Route path="/" exact element={<HomePage />} />
       </Routes>
