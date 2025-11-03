8f536734ae3d784653c0ac7d2f881a3b08041347
ChengAaron <cheng.aaron@outlook.com>
2025-11-03 11:57:37 -0500

    updates

--- src/components/constants.js
+++ src/components/constants.js
@@ -1,7 +1,7 @@
 export const text = [
     {firstName: "AARON "},
     {lastName: "CHENG"},
-    {bio: "ITEC @ YorkU | Semi-Competitive CFV and OPTCG Player | Mechanical Keyboard Nerd | Coffee & Tea Enthusiast | Interested in Data Analytics and Full-Stack Dev"},
+    {bio: "ITEC @ YorkU | Semi-Competitive Gundam TCG Player | Mechanical Keyboard Nerd | Coffee & Tea Enthusiast | Interested in PM, Big Data, Full-Stack and Enterprize Software Dev"},
     {desc: "also known as chengs on discord"},
 ]
 
@@ -18,6 +18,7 @@ export const siteLinks = [
     {title: "Peripherals",  link: "./soon"},
     {title: "Coffee & Tea", link: "./soon"},
     {title: "Projects", link: "./projects"},
+    {title: "Blog", link: "./soon"}
 ]
 
 export const vgDecks = [
@@ -30,7 +31,7 @@ export const projects = [
     {projectName: "F1 Dashboard", projectLink: "https://f1-dashboard-alpha.vercel.app/", projectDesc: "Dashboard made with PowerBI utilizing Excel, Dax and PowerQuery to analyze F1 data from the 1950 to 2024 seasons."},
     {projectName: "Pype", projectLink: "https://github.com/RickyuXen/CHDHack", projectDesc: "Project developed for CTRL-HACK-DEL at YorkU, interpreted language built upon Python. Capable of performing simple arithmetic, variable assignment, and string manipulation."},
     {projectName: "NYC Amusement Park Analysis", projectLink: "https://github.com/ChengAaron/1130-Final", projectDesc: "Final project for university class, determining the viability of an amusement park in NYC based on demographic data with over 300,000 unique data points. Utilized pandas and numpy to perform A/B analysis, data cleaning, and data visualization."},
-    {projectName: "Rice Data Analysis", projectLink:"https://docs.google.com/document/d/16CW2PEl3zqXL-1toXJl6C3nGrn50e9VXkvEMc-DK5xc/edit?usp=sharing", projectDesc: "Final project for university class, comparing 2 species of rice using over 6000 data points. Utilizaed MATLAB to perform A/B analysis, data cleaning, and data visualization"},
+    {projectName: "Rice Data Analysis", projectLink:"https://docs.google.com/document/d/16CW2PEl3zqXL-1toXJl6C3nGrn50e9VXkvEMc-DK5xc/edit?usp=sharing", projectDesc: "Final project for university class, comparing 2 species of rice using over 6000 data points. Utilized MATLAB to perform A/B analysis, data cleaning, and data visualization"},
     {projectName: "Morse", projectLink: "https://github.com/Surfs-Up", projectDesc: "Project developed for Surfs-Up Hacks 2021. Developed input device using Raspberry Pi and Arduino for Morse code input and connected frontend interface to backend server."}
 ]
 
